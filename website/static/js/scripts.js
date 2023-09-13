41154d85b63e03653895ce6ab074c5bf460a3e77
samahesh56 <samahesh56@gmail.com>
2023-09-12 23:48:06 -0400

    slider preview

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -17,3 +17,43 @@ document.addEventListener('DOMContentLoaded', function() { //implementation for 
     });
 });
 
+// Get references to HTML elements
+const slider = document.querySelector(".slider");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+let currentSlide = 0;
+
+// Initially hide all slides except the first one
+const slides = document.querySelectorAll(".slider > *");
+for (let i = 1; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+
+// Add event listeners to the buttons
+prevBtn.addEventListener("click", () => {
+    showSlide(currentSlide - 1);
+});
+
+nextBtn.addEventListener("click", () => {
+    showSlide(currentSlide + 1);
+});
+
+function showSlide(slideIndex) {
+    // Hide the current slide
+    slides[currentSlide].style.display = "none";
+
+    if (slideIndex >= slides.length) {
+        currentSlide = 0;
+    } else if (slideIndex < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = slideIndex;
+    }
+
+    // Show the new current slide
+    slides[currentSlide].style.display = "block";
+}
+
+// Initially, show the first slide
+showSlide(currentSlide);
